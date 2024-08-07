cf5bc4fed6c43aa535ded913885b51f85b629990
Richi <fitrarizki23@gmail.com>
2024-08-07 14:14:47 +0700

    Service discovery

--- order-service/src/main/java/tech/cyberboy/order_service/service/OrderService.java
+++ order-service/src/main/java/tech/cyberboy/order_service/service/OrderService.java
@@ -21,7 +21,7 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -33,8 +33,8 @@ public class OrderService {
         List<String> skuCodes = orderLineItems.stream().map(OrderLineItem::getSkuCode).toList();
 
         // Call Inventory Service and place order if the product is in stock
-        InventoryResponse[] inventoryResponses = webClient.get()
-                .uri("http://localhost:8082/api/inventory",
+        InventoryResponse[] inventoryResponses = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory",
                         uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
